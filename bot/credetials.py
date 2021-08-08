eea8100308b2feae76a15a0abccae658eba73e13
Muhammadkarim <muhammadkarim2308icloud.com@MacBook-Pro-Asilbek.local>
2021-08-09 02:04:02 +0500

    first committta

--- bot/credetials.py
+++ bot/credetials.py
@@ -1,3 +1,3 @@
 TOKEN = '1943274544:AAHj9-0VIll2CQ6hWOv_MqGK4KPmf3jNqkQ'
-URL = 'https://hidden-cliffs-15372.herokuapp.com/getpost/'
+URL = 'https://telegram-bot-asilbek.herokuapp.com/getpost/'
 BOT_API = f'https://api.telegram.org/bot{TOKEN}/'
